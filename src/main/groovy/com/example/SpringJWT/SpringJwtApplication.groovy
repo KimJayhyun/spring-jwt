2ccf7938711a0dcd451edd641d746e27f6aaf21d
jhkim_galaxy_book <dnlwjem000@gmail.com>
2024-05-15 19:26:44 +0900

    Init Repo : 7 장

---
+++ src/main/groovy/com/example/SpringJWT/SpringJwtApplication.groovy
@@ -0,0 +1,15 @@
+package com.example.SpringJWT
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringJwtApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringJwtApplication, args)
+	}
+
+}
+
+
